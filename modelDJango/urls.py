8c018df5f52c5271e535777859f8a51e062ae905
Laury4005 <laurypd@gmail.com>
2024-05-05 12:22:23 -0500

    Autenticación db DJango SQLite, protección form ataques CSRF, desarrollo ApiRest, cambio de estructura, ajustes de urls y formulario login

---
+++ modelDJango/urls.py
@@ -0,0 +1,13 @@
+from . import views
+from django.views.generic import TemplateView
+from django.urls import path
+
+urlpatterns = [
+    #path('admin/', admin.site.urls),
+    path('', TemplateView.as_view(template_name='login.html'), name='home'),
+    path('accounts/login/', views.user_login, name='user_login'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('authors/', views.authors, name='authors'),
+    path('api/authors/', views.author_list),
+    path('api/authors/<int:pk>/', views.author_detail),
+]
